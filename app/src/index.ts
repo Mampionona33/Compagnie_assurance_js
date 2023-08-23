d0970878ab29987bdad81b5794f99cb8a14926cc
Mampionona33 <ramamps33@gmail.com>
2023-08-23 12:12:37 +0000

    finish get nom prenom from modal

--- app/src/index.ts
+++ app/src/index.ts
@@ -2,53 +2,86 @@ import * as bootstrap from "bootstrap";
 import { Table } from "./ts/components/Table";
 import { CustomModal } from "./ts/components/CustomModal";
 import { FormAjoutConducteur } from "./ts/components/FormAjoutConducteur";
+import * as $ from "jquery";
 
-console.log("Hello World!");
+class App {
+  formAjoutConducteur: FormAjoutConducteur;
+  modal;
 
-const table = new Table();
-table.setHeader([
-  "Nom",
-  "Prénom",
-  "Date de naissance",
-  "age",
-  "dete optention permis",
-  "date d'adhésion",
-  "nombre d'accident",
-  "tarif",
-]);
+  constructor() {
+    console.log("Hello World!");
+    this.formAjoutConducteur = new FormAjoutConducteur();
+    const table = new Table();
+    table.setHeader([
+      "Nom",
+      "Prénom",
+      "Date de naissance",
+      "age",
+      "dete optention permis",
+      "date d'adhésion",
+      "nombre d'accident",
+      "tarif",
+    ]);
 
-const showModal = (modalEl: HTMLElement) => {
-  const modal = new bootstrap.Modal(modalEl, {
-    backdrop: true,
-    keyboard: true,
-  });
-  modalEl.classList.add("show");
-  modal.show();
-};
+    const modalAddConductorContainer = document.createElement("div");
+    modalAddConductorContainer.classList.add("modal");
+    modalAddConductorContainer.id = "modal";
 
-const handleClickButtonAdd = (modalEl: HTMLElement) => {
-  const bouttonAddConductor = document.querySelector("#table-btn-add");
-  if (bouttonAddConductor) {
-    bouttonAddConductor.addEventListener("click", (ev: Event) => {
-      ev.preventDefault();
-      showModal(modalEl);
+    const modalAddContent = new CustomModal("Ajout conducteur");
+    // const formAjoutConducteur = new FormAjoutConducteur();
+    modalAddContent.setBody(this.formAjoutConducteur.render());
+    modalAddConductorContainer.innerHTML = modalAddContent.render();
+
+    const bouttonAddConductor = document.querySelector("#table-btn-add");
+    if (bouttonAddConductor) {
+      bouttonAddConductor.addEventListener(
+        "click",
+        this.handleButtonClick.bind(this, modalAddConductorContainer)
+      );
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+      const root = document.querySelector("#root");
+
+      if (root) {
+        root.innerHTML = table.render();
+        document.body.appendChild(modalAddConductorContainer);
+        this.handleClickModalSave();
+      }
     });
   }
-};
 
-document.addEventListener("DOMContentLoaded", () => {
-  const root = document.querySelector("#root");
-  const modalAddConductorContainer = document.createElement("div");
-  modalAddConductorContainer.classList.add("modal");
-
-  const modalAddContent = new CustomModal("Ajout conducteur");
-  const formAjoutConducteur = new FormAjoutConducteur();
-  modalAddContent.setBody(formAjoutConducteur.render());
-  modalAddConductorContainer.innerHTML = modalAddContent.render();
-
-  if (root) {
-    root.innerHTML = table.render();
-    document.body.appendChild(modalAddConductorContainer);
-    handleClickButtonAdd(modalAddConductorContainer);
+  showModal(modalEl) {
+    this.modal = new bootstrap.Modal(modalEl, {
+      backdrop: true,
+      keyboard: true,
+    });
+    modalEl.classList.add("show");
+    this.modal.show();
   }
-});
+
+  handleButtonClick(modalEl, ev) {
+    ev.preventDefault();
+    this.showModal(modalEl);
+  }
+
+
+  handleClickModalSave() {
+    const saveButton = document.querySelector("#btn_save");
+    if (saveButton) {
+      saveButton.addEventListener("click", () => {
+        const nomInput = document.getElementById("nom") as HTMLInputElement;
+        const prenomInput = document.getElementById("prenom") as HTMLInputElement;
+  
+        const name = nomInput.value;
+        const prenom = prenomInput.value;
+  
+        console.log(name, prenom);
+      });
+    }
+  }
+  
+  
+}
+
+const app = new App();
